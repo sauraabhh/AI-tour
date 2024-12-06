c12f0901b6135124457e0693feabae7b6623f38e
sauraabhh <saurabhsangam2704@gmail.com>
2024-12-06 18:04:01 +0530

    Initial commit

---
+++ src/ui/App.tsx
@@ -0,0 +1,60 @@
+import { Route, Routes } from 'react-router';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <div id="mainpage">
+      <div id="page1">
+        
+        <div id="image-1">
+          <div id="circle"></div>
+        </div>
+
+      </div>
+      <Routes>
+        <Route></Route>
+      </Routes>
+
+      
+
+
+      {/* <div id="page3">
+        <div id="image-3">
+          <div id="navbar">
+            <button>EXPLORE <br />CITY</button>
+            <button>PLAN <br /> ITINERARY</button>
+            <button>BOOKINGS</button>
+            <button>HELP & <br />SUPPORT</button>
+            <button>LANGUAGE</button>
+          </div>
+          <div id="middle">
+            <div className='arrow-3' id="arrow-left"></div>
+            <div className='arrow-3' id="arrow-right"></div>
+          </div>
+          <div id="down-3">
+            <div id="left-3">
+              <h1>Hawa Mahal</h1>
+              <p>Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus expedita delectus natus provident vitae fugit in nemo. Ex facere porro, eveniet omnis quos velit blanditiis maiores in eum dolorem expedita. sit amet consectetur adipisicing elit. Molestiae quo vero repellat assumenda, voluptate quaerat aspernatur autem impedit in ex nisi omnis nam nobis eligendi nesciunt inventore doloribus! Sed, beatae!</p>
+            </div>
+            <div id="right-3">
+              <div className='chatbot' id="chatbot-3">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+
+      <div id="page4">
+
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App;
